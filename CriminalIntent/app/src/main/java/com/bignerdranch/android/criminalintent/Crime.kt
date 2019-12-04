4893b47f12d99a5a3299170d2c520f1ea371e0a1
petrdubrovskiy <13-b0in@mail.ru>
2019-12-04 22:02:13 +0300

    start chapter 8
    insert CrimeFragment to container

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 data class Crime(
     val id: UUID = UUID.randomUUID(),
-    var title: String,
+    var title: String = "",
     var date: Date = Date(),
     var isSolved: Boolean = false
 )
